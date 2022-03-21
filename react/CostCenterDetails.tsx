307d1894686a39a37112bde1b9c9a3f2d5ff96ae
Artur Magalhães <artur.magalhaes@vtex.com.br>
2022-03-21 15:22:37 -0300

    B2BORG-80
    Added default shipping address to my account

--- react/CostCenterDetails.tsx
+++ react/CostCenterDetails.tsx
@@ -1,3 +1,7 @@
-import CostCenterDetails from './admin/CostCenterDetails'
+// import CostCenterDetails from './admin/CostCenterDetails'
+
+import React from 'react'
+
+const CostCenterDetails = () => <React.Fragment>HERE</React.Fragment>
 
 export default CostCenterDetails
