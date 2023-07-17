57ea5680312d11e7f8b386894ccf92409a4ceb23
Maíra Bello <maira.bello@vtex.com.br>
2023-07-17 10:23:32 -0300

    Update styles of the organizations list table

--- react/admin/OrganizationsList.tsx
+++ react/admin/OrganizationsList.tsx
@@ -1,344 +1,135 @@
-import type { ChangeEvent, FunctionComponent } from 'react'
-import React, { Fragment, useState } from 'react'
-import { Checkbox, Table, Tag } from 'vtex.styleguide'
+import React, { useEffect } from 'react'
+import type { FunctionComponent } from 'react'
 import { useIntl } from 'react-intl'
+import {
+  createColumns,
+  csx,
+  DataView,
+  IconEye,
+  Table,
+  Tag,
+  THead,
+  THeadCell,
+  TBody,
+  TBodyRow,
+  TBodyCell,
+  useDataViewState,
+  useTableState,
+  Skeleton,
+} from '@vtex/admin-ui'
+import type { TagProps } from '@vtex/admin-ui'
 import { useRuntime } from 'vtex.render-runtime'
 
 import { organizationMessages as messages } from './utils/messages'
-import {
-  INITIAL_FETCH_LIST_OPTIONS,
-  useOrganizationsList,
-} from '../organizations/hooks'
+import { useOrganizationsList } from '../organizations/hooks'
 
-interface CellRendererProps {
-  cellData: unknown
-  rowData: OrganizationSimple
-  updateCellMeasurements: () => void
+export const TagVariantByStatus: Record<string, TagProps['variant']> = {
+  active: 'green',
+  inactive: 'red',
+  'on-hold': 'orange',
 }
-
 interface OrganizationSimple {
   id: string
   name: string
   status: string
 }
 
-export const labelTypeByStatusMap: Record<string, string> = {
-  active: 'success',
-  inactive: 'error',
-  'on-hold': 'warning',
-}
-
 const OrganizationsList: FunctionComponent = () => {
   const { formatMessage } = useIntl()
   const { navigate } = useRuntime()
 
-  const [filterState, setFilterState] = useState({
-    filterStatements: [] as FilterStatement[],
-  })
-
-  const [variableState, setVariables] = useState(INITIAL_FETCH_LIST_OPTIONS)
-
-  const { data, loading, refetch } = useOrganizationsList()
-
-  const getSchema = () => ({
-    properties: {
-      name: {
-        title: formatMessage(messages.tableColumnName),
-      },
-      status: {
-        title: formatMessage(messages.columnStatus),
-        cellRenderer: ({ rowData: { status } }: CellRendererProps) => (
-          <Tag type={labelTypeByStatusMap[status]}>{status}</Tag>
-        ),
-        sortable: true,
-      },
-    },
-  })
-
-  const statusSelectorObject = ({
-    value,
-    onChange,
-  }: {
-    value: Record<string, unknown>
-    onChange: any
-  }) => {
-    const initialValue = {
-      active: true,
-      'on-hold': true,
-      inactive: true,
-      ...(value || {}),
-    } as Record<string, unknown>
-
-    const toggleValueByKey = (key: string) => {
-      return {
-        ...(value || initialValue),
-        [key]: value ? !value[key] : false,
-      }
-    }
-
-    return (
-      <div>
-        {Object.keys(initialValue).map((opt, index) => {
-          return (
-            <div className="mb3" key={`status-select-object-${opt}-${index}`}>
-              <Checkbox
-                checked={value ? value[opt] : initialValue[opt]}
-                label={opt}
-                name="status-checkbox-group"
-                onChange={() => {
-                  const newValue = toggleValueByKey(`${opt}`)
-                  const newValueKeys = Object.keys(newValue)
-                  const isEmptyFilter = !newValueKeys.some(
-                    key => !newValue[key]
-                  )
-
-                  onChange(isEmptyFilter ? null : newValue)
-                }}
-                value={opt}
-              />
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
-
-  const handlePrevClick = () => {
-    if (variableState.page === 1) return
-
-    const newPage = variableState.page - 1
-
-    setVariables({
-      ...variableState,
-      page: newPage,
-    })
-
-    refetch({
-      ...variableState,
-      page: newPage,
-    })
-  }
-
-  const handleNextClick = () => {
-    const newPage = variableState.page + 1
-
-    setVariables({
-      ...variableState,
-      page: newPage,
-    })
-
-    refetch({
-      ...variableState,
-      page: newPage,
-    })
-  }
-
-  const handleRowsChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = e
-
-    setVariables({
-      ...variableState,
-      page: 1,
-      pageSize: +value,
-    })
-
-    refetch({
-      ...variableState,
-      page: 1,
-      pageSize: +value,
-    })
-  }
-
-  const handleFiltersChange = (statements: FilterStatement[]) => {
-    const statuses = [] as string[]
-
-    statements.forEach(statement => {
-      if (!statement?.object) return
-      const { subject, object } = statement
-
-      if (subject !== 'status' || !object) {
-        return
-      }
-
-      const keys = Object.keys(object)
-      const isAllTrue = !keys.some(key => !object[key])
-      const isAllFalse = !keys.some(key => object[key])
-      const trueKeys = keys.filter(key => object[key])
-
-      if (isAllTrue) return
-      if (isAllFalse) statuses.push('none')
-      statuses.push(...trueKeys)
-    })
-
-    setFilterState({
-      filterStatements: statements,
-    })
-
-    setVariables({
-      ...variableState,
-      status: statuses,
-      page: 1,
-    })
-
-    refetch({
-      ...variableState,
-      page: 1,
-      status: statuses,
-    })
-  }
-
-  const handleInputSearchChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e
-
-    setVariables({
-      ...variableState,
-      search: value,
-    })
-  }
-
-  const handleInputSearchClear = () => {
-    setVariables({
-      ...variableState,
-      search: '',
-    })
-
-    refetch({
-      ...variableState,
-      search: '',
-      page: 1,
-    })
-  }
-
-  const handleInputSearchSubmit = () => {
-    refetch({
-      ...variableState,
-      page: 1,
-    })
-  }
-
-  const onRowClick = ({ rowData: { id } }: CellRendererProps) => {
-    if (!id) return
-
-    navigate({
-      page: 'admin.app.b2b-organizations.organization-details',
-      params: { id },
-    })
-  }
-
-  const handleSort = ({
-    sortOrder: _sortOrder,
-    sortedBy: _sortedBy,
-  }: {
-    sortOrder: string
-    sortedBy: string
-  }) => {
-    setVariables({
-      ...variableState,
-      sortOrder: _sortOrder,
-      sortedBy: _sortedBy,
-    })
-    refetch({
-      ...variableState,
-      page: 1,
-      sortOrder: _sortOrder,
-      sortedBy: _sortedBy,
-    })
-  }
-
-  const lineActions = [
+  const columns = createColumns<OrganizationSimple>([
     {
-      label: () => formatMessage(messages.view),
-      onClick: onRowClick,
+      id: 'name',
+      header: formatMessage(messages.tableColumnName),
+      width: '3fr',
+      resolver: {
+        type: 'text',
+        columnType: 'name',
+        mapText: ({ name }) => name,
+        render: ({ data }) => (
+          <div className={csx({ minWidth: '10rem' })}>{data}</div>
+        ),
+      },
     },
-  ]
+    {
+      id: 'status',
+      header: formatMessage(messages.columnStatus),
+      resolver: {
+        type: 'root',
+        render: ({ item, context }) => {
+          if (context === 'loading') {
+            return <Skeleton className={csx({ height: '1.5rem' })} />
+          }
 
-  const { total } = data?.getOrganizations?.pagination ?? 0
-  const { page, pageSize, search, sortedBy, sortOrder } = variableState
+          return (
+            <Tag
+              label={item.status}
+              size="normal"
+              variant={TagVariantByStatus[item.status]}
+            />
+          )
+        },
+      },
+    },
+    {
+      id: 'menu',
+      resolver: {
+        type: 'menu',
+        actions: [
+          {
+            label: formatMessage(messages.view),
+            icon: <IconEye />,
 
-  return (
-    <Fragment>
-      <Table
-        fullWidth
-        schema={getSchema()}
-        fixFirstColumn
-        loading={loading}
-        emptyStateLabel={formatMessage(messages.organizationsEmptyState)}
-        items={data?.getOrganizations?.data}
-        lineActions={lineActions}
-        onRowClick={onRowClick}
-        pagination={{
-          onNextClick: handleNextClick,
-          onPrevClick: handlePrevClick,
-          onRowsChange: handleRowsChange,
-          currentItemFrom: (page - 1) * pageSize + 1,
-          currentItemTo: total < page * pageSize ? total : page * pageSize,
-          textShowRows: formatMessage(messages.showRows),
-          textOf: formatMessage(messages.of),
-          totalItems: total ?? 0,
-          rowsOptions: [25, 50, 100],
-        }}
-        toolbar={{
-          inputSearch: {
-            value: search,
-            placeholder: formatMessage(messages.searchPlaceholder),
-            onChange: handleInputSearchChange,
-            onClear: handleInputSearchClear,
-            onSubmit: handleInputSearchSubmit,
-          },
-        }}
-        sort={{
-          sortedBy,
-          sortOrder,
-        }}
-        onSort={handleSort}
-        filters={{
-          alwaysVisibleFilters: ['status'],
-          statements: filterState.filterStatements,
-          onChangeStatements: handleFiltersChange,
-          clearAllFiltersButtonLabel: formatMessage(messages.clearFilters),
-          collapseLeft: true,
-          options: {
-            status: {
-              label: formatMessage(messages.filterStatus),
-              renderFilterLabel: (st: any) => {
-                if (!st || !st.object) {
-                  // you should treat empty object cases only for alwaysVisibleFilters
-                  return formatMessage(messages.filtersAll)
-                }
-
-                const keys = st.object ? Object.keys(st.object) : []
-                const isAllTrue = !keys.some(key => !st.object[key])
-                const isAllFalse = !keys.some(key => st.object[key])
-                const trueKeys = keys.filter(key => st.object[key])
-                let trueKeysLabel = ''
-
-                trueKeys.forEach((key, index) => {
-                  trueKeysLabel += `${key}${
-                    index === trueKeys.length - 1 ? '' : ', '
-                  }`
-                })
-
-                if (isAllTrue) return formatMessage(messages.filtersAll)
-                if (isAllFalse) return formatMessage(messages.filtersNone)
-
-                return trueKeysLabel.toString()
-              },
-              verbs: [
-                {
-                  label: formatMessage(messages.filtersIncludes),
-                  value: 'includes',
-                  object: statusSelectorObject,
-                },
-              ],
+            onClick: ({ id }) => {
+              navigate({
+                page: 'admin.app.b2b-organizations.organization-details',
+                params: { id },
+              })
             },
           },
-        }}
-      />
-    </Fragment>
+        ],
+      },
+    },
+  ])
+
+  const { data: fetchedOrgs, loading } = useOrganizationsList()
+
+  const view = useDataViewState()
+  const { data, getBodyCell, getHeadCell, getTable } = useTableState({
+    status: view.status,
+    columns,
+    items: fetchedOrgs?.getOrganizations?.data,
+    length: 10,
+  })
+
+  useEffect(() => {
+    view.setStatus({
+      type: loading ? 'loading' : 'ready',
+    })
+  }, [loading])
+
+  return (
+    <DataView state={view} className={csx({ paddingX: '$space-3' })}>
+      <Table width="100%" {...getTable()}>
+        <THead>
+          {columns.map(column => (
+            <THeadCell {...getHeadCell(column)} />
+          ))}
+        </THead>
+        <TBody>
+          {data?.map(item => {
+            return (
+              <TBodyRow key={item.id}>
+                {columns.map(column => {
+                  return <TBodyCell {...getBodyCell(column, item)} />
+                })}
+              </TBodyRow>
+            )
+          })}
+        </TBody>
+      </Table>
+    </DataView>
   )
 }
 
