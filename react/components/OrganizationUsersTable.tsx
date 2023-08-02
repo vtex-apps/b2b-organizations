614fa9e847501462c5caa4bc059f16beac8e4456
Mauro Takeda <mauro.takeda@vtex.com.br>
2023-08-02 16:28:53 -0300

    Use get session to get data from impersonate user

--- react/components/OrganizationUsersTable.tsx
+++ react/components/OrganizationUsersTable.tsx
@@ -458,7 +458,7 @@ const OrganizationUsersTable: FunctionComponent<Props> = ({
   }
 
   const handleNextClick = () => {
-    const newPage = 1 // variableState.page + 1
+    const newPage = variableState.page + 1
 
     setVariables({
       ...variableState,
