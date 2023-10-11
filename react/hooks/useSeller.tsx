1327e417a465bfcaf76f7cd1d4ffc3080434ad91
Artur Magalhães <nezkal@gmail.com>
2023-10-11 13:29:07 -0300

    adding useSeller

---
+++ react/hooks/useSeller.tsx
@@ -0,0 +1,30 @@
+// Dependencies
+import { useState, useEffect } from 'react'
+import { useProduct } from 'vtex.product-context'
+
+const decode = (str: string): string =>
+  Buffer.from(str, 'base64').toString('binary')
+
+export default function useSeller() {
+  const [seller, setSeller] = useState<string | null>(null)
+  const { selectedItem, product } = useProduct() ?? {}
+
+  useEffect(() => {
+    try {
+      const { facets } = JSON.parse(decode(window?.__RUNTIME__?.segmentToken))
+
+      if (facets) {
+        const facetsList = facets.split(';')
+        const sellerFacet = facetsList.find((facet: string) =>
+          facet.includes('private-seller')
+        )
+
+        sellerFacet && setSeller(sellerFacet?.split('=')[1])
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }, [selectedItem, product])
+
+  return { seller }
+}
