afcfe6ca6150cc594703ed0f156565191fe877aa
Josmar Soares Trigueiro Junior <josmar.junior@cubos.io>
2024-10-17 17:33:16 -0300

    feat: add warning topbar when inactive organization

---
+++ react/components/WarningTopbarInactiveOrganization.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useQuery } from 'react-apollo'
+import { FormattedMessage } from 'react-intl'
+
+import GET_B2B_SETTINGS from '../graphql/getB2BSettings.graphql'
+
+function getContrastColor(hexColor: string) {
+  const color = hexColor.replace('#', '')
+
+  const r = parseInt(color.substring(0, 2), 16)
+  const g = parseInt(color.substring(2, 4), 16)
+  const b = parseInt(color.substring(4, 6), 16)
+
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000
+
+  return brightness > 128 ? '#000000' : '#FFFFFF'
+}
+
+const WarningTopbarInactiveOrganization = () => {
+  const { data, loading } = useQuery(GET_B2B_SETTINGS, {
+    ssr: false,
+  })
+
+  if (loading || data?.getB2BSettings?.uiSettings?.autoApprove) {
+    return <></>
+  }
+
+  return (
+    <div
+      className="pv3"
+      style={{
+        width: '100%',
+        textAlign: 'center',
+        fontWeight: 500,
+        color: getContrastColor(
+          data?.getB2BSettings?.uiSettings?.topBar?.hexColor ?? '#656896'
+        ),
+        background:
+          data?.getB2BSettings?.uiSettings?.topBar?.hexColor ?? '#656896',
+      }}
+    >
+      {data?.getB2BSettings?.uiSettings?.topBar?.name || (
+        <FormattedMessage id="store/b2b-organizations.warning-topbar-inative-org.message" />
+      )}
+    </div>
+  )
+}
+
+export default WarningTopbarInactiveOrganization
