94e51778ff022ceb22479476c597cde789902156
Wender Lima <wender.lima@vtex.com.br>
2026-04-15 17:23:10 -0400

    Adding user's authentication to the permissions request

--- react/hooks/useOrgPermission.ts
+++ react/hooks/useOrgPermission.ts
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useState } from 'react'
 import useSWR from 'swr'
 import { useFullSession } from 'vtex.session-client'
 
@@ -9,22 +10,47 @@ interface UseOrgPermissionParams {
   resourceCode?: typeof ORGANIZATION_EDIT | typeof ORGANIZATION_VIEW
 }
 
+const VTEX_ID_CLIENT_AUT_COOKIE_KEY_PREFIX = 'VtexIdclientAutCookie'
+
+const BASE_SESSION_ITEMS = [
+  'authentication.adminUserEmail',
+  'authentication.storeUserEmail',
+  'profile.email',
+  'account.accountName',
+] as const
+
 export function useOrgPermission({
   resourceCode = ORGANIZATION_VIEW,
 }: UseOrgPermissionParams) {
+  const [accountForSessionItems, setAccountForSessionItems] = useState<
+    string | undefined
+  >()
+
+  const sessionItems = useMemo(() => {
+    if (!accountForSessionItems) {
+      return [...BASE_SESSION_ITEMS]
+    }
+
+    return [
+      ...BASE_SESSION_ITEMS,
+      `cookie.${VTEX_ID_CLIENT_AUT_COOKIE_KEY_PREFIX}_${accountForSessionItems}`,
+    ]
+  }, [accountForSessionItems])
+
   const fullSession = useFullSession({
     variables: {
-      items: [
-        'authentication.adminUserEmail',
-        'authentication.storeUserEmail',
-        'profile.email',
-        'account.accountName',
-      ],
+      items: sessionItems,
     },
   })
 
   const account =
     fullSession.data?.session?.namespaces?.account?.accountName?.value
+
+  useEffect(() => {
+    if (account && account !== accountForSessionItems) {
+      setAccountForSessionItems(account)
+    }
+  }, [account, accountForSessionItems])
 
   const namespaces = fullSession.data?.session?.namespaces
 
@@ -36,13 +62,21 @@ export function useOrgPermission({
 
   const userEmail = adminUserEmail || storeUserEmail || profileEmail
 
+  const vtexAuthCookie = account
+    ? namespaces?.cookie?.[`${VTEX_ID_CLIENT_AUT_COOKIE_KEY_PREFIX}_${account}`]
+        ?.value
+    : undefined
+
   const { data, isLoading, isValidating, error } = useSWR<{ data: boolean }>(
-    userEmail && account ? `/granted?${resourceCode}` : null,
+    userEmail && account && vtexAuthCookie
+      ? `/granted?${resourceCode}`
+      : null,
     () =>
       checkUserAdminPermission({
         account,
         userEmail,
         resourceCode,
+        authCookie: vtexAuthCookie,
       }),
     {
       dedupingInterval: 0,
--- react/services/orgPermissionClient.ts
+++ react/services/orgPermissionClient.ts
@@ -3,22 +3,36 @@ import axios from 'axios'
 import { B2B_LM_PRODUCT_CODE } from '../utils/constants'
 
 const LICENSE_MANAGER_BASE_URL = `/api/license-manager/pvt/accounts/`
+const VTEX_ID_CLIENT_AUT_COOKIE_PREFIX = 'VtexIdclientAutCookie'
 
 const orgPermissionClient = axios.create()
 
 orgPermissionClient.defaults.baseURL = LICENSE_MANAGER_BASE_URL
+orgPermissionClient.defaults.withCredentials = true
 
 interface CheckUserAdminPermissionParams {
   account: string
   userEmail: string
   resourceCode: string
+  authCookie: string
 }
 
 const checkUserAdminPermission = async ({
   account,
   userEmail,
   resourceCode,
+  authCookie,
 }: CheckUserAdminPermissionParams) => {
+  if (authCookie) {
+    orgPermissionClient.defaults.headers.common[
+      VTEX_ID_CLIENT_AUT_COOKIE_PREFIX
+    ] = authCookie
+  } else {
+    delete orgPermissionClient.defaults.headers.common[
+      VTEX_ID_CLIENT_AUT_COOKIE_PREFIX
+    ]
+  }
+
   const checkOrgPermission = await orgPermissionClient.get(
     `${account}/products/${B2B_LM_PRODUCT_CODE}/logins/${userEmail}/resources/${resourceCode}/granted`
   )
